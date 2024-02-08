433969d1d9c577d3910d81f1ae73b787ded70657
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2024-02-08 17:39:44 +0000

    pomodoro timer finished

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useState, useReducer } from 'react'
+import {
+  ReactNode,
+  createContext,
+  useState,
+  useReducer,
+  useEffect,
+} from 'react'
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducers'
 import {
   ActionTypes,
@@ -6,6 +12,8 @@ import {
   interruptCurrentCycleAction,
   markCurrentAsFinishedAction,
 } from '../reducers/cycles/actions'
+import { effect } from 'zod'
+import differenceInSeconds from 'date-fns/differenceInSeconds'
 
 interface CreateCycleData {
   task: string
@@ -32,15 +40,38 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateAsJson = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0'
+      )
+      if (storedStateAsJson) {
+        return JSON.parse(storedStateAsJson)
+      }
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+      return initialState
+    }
+  )
 
   const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+    return 0
+  })
+  useEffect(() => {
+    const stateJson = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJson)
+  }, [cyclesState])
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
